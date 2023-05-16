51bcb62af2770ecde321ff15689f88d272fec1ba
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-16 19:46:07 +0600

    Page, router and navbar added

---
+++ src/Router/router.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layouts/Main';
+import Shop from '../Pages/Shop/Shop';
+import Home from '../Pages/Home/Home';
+import MyOrders from '../Pages/MyOrders/MyOrders';
+import OrderReview from '../Pages/OrderReview/OrderReview';
+import Login from '../Pages/Login/Login';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+          path:"/",
+          element:<Home></Home>
+        },
+        {
+          path:"shop",
+          element:<Shop></Shop>
+        },
+        {
+          path:"order",
+          element:<MyOrders></MyOrders>
+        },
+        {
+          path:"orderReview",
+          element:<OrderReview></OrderReview>
+        },
+        {
+          path: "login",
+          element:<Login></Login>
+        }
+      ]
+    },
+  ]);
+  
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Auth from './Auth/Auth.jsx'
+import router from './Router/router.jsx'
+import {
+  RouterProvider
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
     <React.StrictMode>
       <Auth>
-        <App />
+      <RouterProvider router={router} />
       </Auth>
     </React.StrictMode>,
   </div>
