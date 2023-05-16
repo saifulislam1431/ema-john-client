9ca3ea065a89593fc1225c57012161430b1186f3
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-16 20:25:01 +0600

    Home component added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import heroImg from "../../assets/images/heroImg.jpg"
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='mt-16'>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-14">
+                    <div className='bg-secondary bg-opacity-40 rounded-lg w-full lg:w-auto'>
+                    <img src={heroImg} className="rounded-lg relative left-4 top-4 w-full lg:max-w-sm" />
+                    </div>
+                    <div>
+                        <p className='my-10 text-primary'>Sale up to 70% off</p>
+                        <h1 className="text-5xl font-bold">New Collection For Fall</h1>
+                        <p className="py-6">Discover all the new arrivals of ready-to-wear collection.</p>
+                        <Link to="/shop">
+                        <button className="btn btn-primary">Shop Now</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,34 +3,36 @@ import logo from "../../../assets/images/Logo.svg"
 import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-accent text-white mt-10">
-  <div>
-    <Link to="/">
-    <img src={logo} alt="Logo" />
-    </Link>
-    <p>Ema John Ltd.<br/>Providing reliable e-shopping since 2023</p>
-  </div> 
-  <div>
-    <span className="footer-title">Services</span> 
-    <Link to= "/" className="link link-hover">Branding</Link> 
-    <Link to= "/" className="link link-hover">Design</Link> 
-    <Link to= "/" className="link link-hover">Marketing</Link> 
-    <Link to= "/" className="link link-hover">Advertisement</Link>
-  </div> 
-  <div>
-    <span className="footer-title">Company</span> 
-    <Link to= "/" className="link link-hover">About us</Link> 
-    <Link to= "/" className="link link-hover">Contact</Link> 
-    <Link to= "/" className="link link-hover">Jobs</Link> 
-    <Link to= "/" className="link link-hover">Press kit</Link>
-  </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <Link to= "/" className="link link-hover">Terms of use</Link> 
-    <Link to= "/" className="link link-hover">Privacy policy</Link> 
-    <Link to= "/" className="link link-hover">Cookie policy</Link>
-  </div>
-</footer>
+        <footer>
+           <div className="footer p-14 bg-accent text-white mt-14">
+           <div>
+                <Link to="/">
+                    <img src={logo} alt="Logo" />
+                </Link>
+                <p>Ema John Ltd.<br />Providing reliable e-shopping since 2023</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <Link to="/" className="link link-hover">Branding</Link>
+                <Link to="/" className="link link-hover">Design</Link>
+                <Link to="/" className="link link-hover">Marketing</Link>
+                <Link to="/" className="link link-hover">Advertisement</Link>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <Link to="/" className="link link-hover">About us</Link>
+                <Link to="/" className="link link-hover">Contact</Link>
+                <Link to="/" className="link link-hover">Jobs</Link>
+                <Link to="/" className="link link-hover">Press kit</Link>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <Link to="/" className="link link-hover">Terms of use</Link>
+                <Link to="/" className="link link-hover">Privacy policy</Link>
+                <Link to="/" className="link link-hover">Cookie policy</Link>
+            </div>
+           </div>
+        </footer>
     );
 };
 
