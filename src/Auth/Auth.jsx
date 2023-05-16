f035e35239e40639720e136eb9f4c5c8e56ee2dd
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-16 19:16:49 +0600

    Auth provider added

--- src/Auth/Auth.jsx
+++ src/Auth/Auth.jsx
@@ -1,14 +1,53 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const UserContext = createContext(null)
 const auth = getAuth(app);
 
-const Auth = () => {
+const Auth = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const sigUp = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const sigIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleLog = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setLoading(false);
+            setUser(currentUser);
+        })
+        return () => unsubscribe();
+    }, [])
+
+
+    const userInfo = {
+        user,
+        loading,
+        sigUp,
+        sigIn,
+        logOut,
+        googleLog
+    }
+
     return (
-        <div>
-            
-        </div>
+        <UserContext.Provider value={userInfo} >
+            {children}
+        </UserContext.Provider>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Auth from './Auth/Auth.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <Auth>
+        <App />
+      </Auth>
+    </React.StrictMode>,
+  </div>
 )
